8d893691f4aa7d3a3fd39900f9387548238f4b07
Istiak Ahmed Saklain <istiakahmedsci00@gmail.com>
2026-04-22 11:13:25 +0600

    22 April 2026

---
+++ LabFinal2/view/settings.php
@@ -0,0 +1,10 @@
+<form method="POST">
+    <label>Select Theme:</label>
+    <select name="theme">
+        <option value="light">Light</option>
+        <option value="dark">Dark</option>
+    </select>
+    <button type="submit">Save</button>
+</form>
+
+<a href="index.php?page=dashboard">Back</a>
