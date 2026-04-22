914c38f6975fc4fb06a4e7681a6488f109d178de
Saklain <istiakahmedsci00@gmail.com>
2026-04-22 10:41:45 +0600

    restore data

--- LabFinal2/view/login.php
+++
@@ -1,6 +0,0 @@
-<form method="POST">
-    Email: <input type="text" name="email"><br>
-    Password: <input type="password" name="password"><br>
-    <span><?= $error ?></span><br>
-    <button type="submit">Login</button>
-</form>
